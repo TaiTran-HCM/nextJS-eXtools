177d277636b5b01e249db4f6ba2bd50c3cf760fb
Tai Tran <tai.tran@weai.vn>
2022-11-11 21:10:14 +0700

    Created box letter and semantic-ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Box from '../components/mail/box'
+import BoxLetter from '../components/mail/box_letter'
 
 export default function Home() {
   return (
-    <Box></Box>
+    <BoxLetter />
   )
 }
